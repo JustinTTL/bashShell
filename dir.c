efe33c5f259d560e4fc7a91bb833617e00d8e459
mcarri01 <matthew.carrington_fair@tufts.edu>
2016-10-21 21:07:45 -0400

    forgot the dir

--- dir.c
+++ dir.c
@@ -1,21 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+void print_dir(char *dirpath);
 
 int main(int argc, char *argv[]) {
-	(void)argc;
+	if (argc == 1) {
+		print_dir("./");
+	} else if (argc > 2){
+		printf("ERROR: Too many arguments\n");
+	} else {
+		print_dir(argv[1]);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+void print_dir(char *dirpath){
 	DIR *dir;
 	struct dirent *ent;
-	if ((dir = opendir (argv[1])) != NULL) {
-	  /* print all the files and directories within directory */
-	  while ((ent = readdir (dir)) != NULL) {
-	    printf ("%s\n", ent->d_name);
-	  }
-	  closedir (dir);
-	} else {
-	  /* could not open directory */
-	  printf("Error: Directory not found\n");
-	  return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+	if ((dir = opendir (dirpath)) != NULL) {
+		  /* print all the files and directories within directory */
+		  while ((ent = readdir (dir)) != NULL) {
+		    printf ("%s\n", ent->d_name);
+		  }
+		  closedir (dir);
+		} else {
+		  /* could not open directory */
+		  printf("Error: Directory not found\n");
+		}
+
 }
